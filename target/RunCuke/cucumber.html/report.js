9c147a56094cb94761e35056787e888543a476c2
Gurdeep <gurukohliopp@gmail.com>
2019-10-31 02:00:03 +0100

    Some comment

--- target/RunCuke/cucumber.html/report.js
+++ target/RunCuke/cucumber.html/report.js
@@ -13,7 +13,7 @@ formatter.feature({
   ]
 });
 formatter.before({
-  "duration": 12475707101,
+  "duration": 10663384500,
   "status": "passed"
 });
 formatter.scenario({
@@ -110,14 +110,14 @@ formatter.match({
   "location": "SearchCarsSteps.i_am_on_the_Home_Page_of_the_CarsGuide_website(String)"
 });
 formatter.result({
-  "duration": 14587189501,
+  "duration": 4392388200,
   "status": "passed"
 });
 formatter.match({
   "location": "SearchCarsSteps.i_move_to_the_menu(String\u003e)"
 });
 formatter.result({
-  "duration": 241131500,
+  "duration": 250818100,
   "status": "passed"
 });
 formatter.match({
@@ -130,7 +130,7 @@ formatter.match({
   "location": "SearchCarsSteps.click_on_link(String)"
 });
 formatter.result({
-  "duration": 4006428200,
+  "duration": 1571259700,
   "status": "passed"
 });
 formatter.match({
@@ -143,7 +143,7 @@ formatter.match({
   "location": "SearchCarsSteps.select_car_brand_as_from_AnyMake_dropdown(String)"
 });
 formatter.result({
-  "duration": 195183300,
+  "duration": 1435616400,
   "status": "passed"
 });
 formatter.match({
@@ -156,7 +156,7 @@ formatter.match({
   "location": "SearchCarsSteps.select_car_model_as_from_AnyModel_dropdown(String)"
 });
 formatter.result({
-  "duration": 683083399,
+  "duration": 874875000,
   "status": "passed"
 });
 formatter.match({
@@ -169,7 +169,7 @@ formatter.match({
   "location": "SearchCarsSteps.select_location_as_from_AnyLocation_dropdown(String)"
 });
 formatter.result({
-  "duration": 140766300,
+  "duration": 227970600,
   "status": "passed"
 });
 formatter.match({
@@ -182,21 +182,21 @@ formatter.match({
   "location": "SearchCarsSteps.select_price_max_as_from_Price_dropdown(String)"
 });
 formatter.result({
-  "duration": 137981200,
+  "duration": 143994400,
   "status": "passed"
 });
 formatter.match({
   "location": "SearchCarsSteps.i_click_on_Find_My_Next_Car_button()"
 });
 formatter.result({
-  "duration": 7339586000,
+  "duration": 1384906700,
   "status": "passed"
 });
 formatter.match({
   "location": "SearchCarsSteps.i_should_see_list_of_searched_cars()"
 });
 formatter.result({
-  "duration": 380999,
+  "duration": 89800,
   "status": "passed"
 });
 formatter.match({
@@ -209,11 +209,11 @@ formatter.match({
   "location": "SearchCarsSteps.the_page_title_should_be(String)"
 });
 formatter.result({
-  "duration": 14616000,
+  "duration": 141280700,
   "status": "passed"
 });
 formatter.after({
-  "duration": 1144886601,
+  "duration": 794601500,
   "status": "passed"
 });
 formatter.uri("UsedCarSearch.feature");
@@ -231,8 +231,9 @@ formatter.feature({
   ]
 });
 formatter.before({
-  "duration": 9688946900,
-  "status": "passed"
+  "duration": 15519960900,
+  "error_message": "org.openqa.selenium.SessionNotCreatedException: session not created\nfrom chrome not reachable\n  (Session info: chrome\u003d78.0.3904.70)\nBuild info: version: \u00273.141.5\u0027, revision: \u0027d54ebd709a\u0027, time: \u00272018-11-06T11:42:16\u0027\nSystem info: host: \u0027DESKTOP-UBRI0MS\u0027, ip: \u0027192.168.23.176\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_221\u0027\nDriver info: driver.version: SeleniumDriver\nremote stacktrace: Backtrace:\n\tOrdinal0 [0x0063A903+1550595]\n\tOrdinal0 [0x005BA701+1025793]\n\tOrdinal0 [0x0053C58E+509326]\n\tOrdinal0 [0x005362C2+484034]\n\tOrdinal0 [0x0053699B+485787]\n\tOrdinal0 [0x00537B05+490245]\n\tOrdinal0 [0x00533A85+473733]\n\tOrdinal0 [0x0053D330+512816]\n\tOrdinal0 [0x004EB649+177737]\n\tOrdinal0 [0x004EA8ED+174317]\n\tOrdinal0 [0x004E8CDB+167131]\n\tOrdinal0 [0x004D144A+70730]\n\tOrdinal0 [0x004D24D0+74960]\n\tOrdinal0 [0x004D2469+74857]\n\tOrdinal0 [0x005D42C7+1131207]\n\tGetHandleVerifier [0x006D70FD+523789]\n\tGetHandleVerifier [0x006D6E90+523168]\n\tGetHandleVerifier [0x006DE1E7+552695]\n\tGetHandleVerifier [0x006D78FA+525834]\n\tOrdinal0 [0x005CB7FC+1095676]\n\tOrdinal0 [0x005D633B+1139515]\n\tOrdinal0 [0x005D64A3+1139875]\n\tOrdinal0 [0x005D5425+1135653]\n\tBaseThreadInitThunk [0x77286359+25]\n\tRtlGetAppContainerNamedObjectPath [0x77A37B74+228]\n\tRtlGetAppContainerNamedObjectPath [0x77A37B44+180]\n\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.W3CHandshakeResponse.lambda$errorHandler$0(W3CHandshakeResponse.java:62)\r\n\tat org.openqa.selenium.remote.HandshakeResponse.lambda$getResponseFunction$0(HandshakeResponse.java:30)\r\n\tat org.openqa.selenium.remote.ProtocolHandshake.lambda$createSession$0(ProtocolHandshake.java:123)\r\n\tat java.util.stream.ReferencePipeline$3$1.accept(ReferencePipeline.java:193)\r\n\tat java.util.Spliterators$ArraySpliterator.tryAdvance(Spliterators.java:958)\r\n\tat java.util.stream.ReferencePipeline.forEachWithCancel(ReferencePipeline.java:126)\r\n\tat java.util.stream.AbstractPipeline.copyIntoWithCancel(AbstractPipeline.java:499)\r\n\tat java.util.stream.AbstractPipeline.copyInto(AbstractPipeline.java:486)\r\n\tat java.util.stream.AbstractPipeline.wrapAndCopyInto(AbstractPipeline.java:472)\r\n\tat java.util.stream.FindOps$FindOp.evaluateSequential(FindOps.java:152)\r\n\tat java.util.stream.AbstractPipeline.evaluate(AbstractPipeline.java:234)\r\n\tat java.util.stream.ReferencePipeline.findFirst(ReferencePipeline.java:464)\r\n\tat org.openqa.selenium.remote.ProtocolHandshake.createSession(ProtocolHandshake.java:125)\r\n\tat org.openqa.selenium.remote.ProtocolHandshake.createSession(ProtocolHandshake.java:74)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:136)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.startSession(RemoteWebDriver.java:213)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.\u003cinit\u003e(RemoteWebDriver.java:131)\r\n\tat org.openqa.selenium.chrome.ChromeDriver.\u003cinit\u003e(ChromeDriver.java:181)\r\n\tat org.openqa.selenium.chrome.ChromeDriver.\u003cinit\u003e(ChromeDriver.java:168)\r\n\tat org.openqa.selenium.chrome.ChromeDriver.\u003cinit\u003e(ChromeDriver.java:123)\r\n\tat utils.SeleniumDriver.\u003cinit\u003e(SeleniumDriver.java:21)\r\n\tat utils.SeleniumDriver.setUpDriver(SeleniumDriver.java:40)\r\n\tat steps.BeforeActions.setUp(BeforeActions.java:12)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:40)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:34)\r\n\tat cucumber.runtime.java.JavaHookDefinition.execute(JavaHookDefinition.java:60)\r\n\tat cucumber.runtime.Runtime.runHookIfTagsMatch(Runtime.java:224)\r\n\tat cucumber.runtime.Runtime.runHooks(Runtime.java:212)\r\n\tat cucumber.runtime.Runtime.runBeforeHooks(Runtime.java:202)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:40)\r\n\tat cucumber.runtime.model.CucumberFeature.run(CucumberFeature.java:165)\r\n\tat cucumber.api.testng.TestNGCucumberRunner.runCucumber(TestNGCucumberRunner.java:63)\r\n\tat cucumber.api.testng.AbstractTestNGCucumberTests.feature(AbstractTestNGCucumberTests.java:21)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat org.testng.internal.MethodInvocationHelper.invokeMethod(MethodInvocationHelper.java:86)\r\n\tat org.testng.internal.Invoker.invokeMethod(Invoker.java:643)\r\n\tat org.testng.internal.Invoker.invokeTestMethod(Invoker.java:820)\r\n\tat org.testng.internal.Invoker.invokeTestMethods(Invoker.java:1128)\r\n\tat org.testng.internal.TestMethodWorker.invokeTestMethods(TestMethodWorker.java:129)\r\n\tat org.testng.internal.TestMethodWorker.run(TestMethodWorker.java:112)\r\n\tat org.testng.TestRunner.privateRun(TestRunner.java:782)\r\n\tat org.testng.TestRunner.run(TestRunner.java:632)\r\n\tat org.testng.SuiteRunner.runTest(SuiteRunner.java:366)\r\n\tat org.testng.SuiteRunner.runSequentially(SuiteRunner.java:361)\r\n\tat org.testng.SuiteRunner.privateRun(SuiteRunner.java:319)\r\n\tat org.testng.SuiteRunner.run(SuiteRunner.java:268)\r\n\tat org.testng.SuiteRunnerWorker.runSuite(SuiteRunnerWorker.java:52)\r\n\tat org.testng.SuiteRunnerWorker.run(SuiteRunnerWorker.java:86)\r\n\tat org.testng.TestNG.runSuitesSequentially(TestNG.java:1244)\r\n\tat org.testng.TestNG.runSuitesLocally(TestNG.java:1169)\r\n\tat org.testng.TestNG.run(TestNG.java:1064)\r\n\tat org.apache.maven.surefire.testng.TestNGExecutor.run(TestNGExecutor.java:295)\r\n\tat org.apache.maven.surefire.testng.TestNGXmlTestSuite.execute(TestNGXmlTestSuite.java:84)\r\n\tat org.apache.maven.surefire.testng.TestNGProvider.invoke(TestNGProvider.java:90)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.invokeProviderInSameClassLoader(ForkedBooter.java:203)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.runSuitesInProcess(ForkedBooter.java:155)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.main(ForkedBooter.java:103)\r\n",
+  "status": "failed"
 });
 formatter.scenario({
   "line": 7,
@@ -348,15 +349,13 @@ formatter.match({
   "location": "SearchCarsSteps.i_am_on_the_Home_Page_of_the_CarsGuide_website(String)"
 });
 formatter.result({
-  "duration": 5408169000,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "SearchCarsSteps.i_move_to_the_menu(String\u003e)"
 });
 formatter.result({
-  "duration": 157509201,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "arguments": [
@@ -368,8 +367,7 @@ formatter.match({
   "location": "UsedCarsSteps.i_click_on_link(String)"
 });
 formatter.result({
-  "duration": 7275866400,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "arguments": [
@@ -381,15 +379,13 @@ formatter.match({
   "location": "SearchCarsSteps.select_car_brand_as_from_AnyMake_dropdown(String)"
 });
 formatter.result({
-  "duration": 182099600,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "UsedCarsSteps.select_car_model(String\u003e)"
 });
 formatter.result({
-  "duration": 538127100,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "arguments": [
@@ -401,8 +397,7 @@ formatter.match({
   "location": "SearchCarsSteps.select_location_as_from_AnyLocation_dropdown(String)"
 });
 formatter.result({
-  "duration": 131042400,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "arguments": [
@@ -414,22 +409,19 @@ formatter.match({
   "location": "SearchCarsSteps.select_price_max_as_from_Price_dropdown(String)"
 });
 formatter.result({
-  "duration": 114457599,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "SearchCarsSteps.i_click_on_Find_My_Next_Car_button()"
 });
 formatter.result({
-  "duration": 6827628500,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "location": "SearchCarsSteps.i_should_see_list_of_searched_cars()"
 });
 formatter.result({
-  "duration": 52400,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.match({
   "arguments": [
@@ -441,11 +433,11 @@ formatter.match({
   "location": "SearchCarsSteps.the_page_title_should_be(String)"
 });
 formatter.result({
-  "duration": 6160800,
-  "status": "passed"
+  "status": "skipped"
 });
 formatter.after({
-  "duration": 782684701,
-  "status": "passed"
+  "duration": 569200,
+  "error_message": "org.openqa.selenium.NoSuchSessionException: Session ID is null. Using WebDriver after calling quit()?\nBuild info: version: \u00273.141.5\u0027, revision: \u0027d54ebd709a\u0027, time: \u00272018-11-06T11:42:16\u0027\nSystem info: host: \u0027DESKTOP-UBRI0MS\u0027, ip: \u0027192.168.23.176\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_221\u0027\nDriver info: driver.version: RemoteWebDriver\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:125)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:609)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.getScreenshotAs(RemoteWebDriver.java:295)\r\n\tat steps.AfterActions.tearDown(AfterActions.java:20)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:40)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:34)\r\n\tat cucumber.runtime.java.JavaHookDefinition.execute(JavaHookDefinition.java:60)\r\n\tat cucumber.runtime.Runtime.runHookIfTagsMatch(Runtime.java:224)\r\n\tat cucumber.runtime.Runtime.runHooks(Runtime.java:212)\r\n\tat cucumber.runtime.Runtime.runAfterHooks(Runtime.java:206)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:46)\r\n\tat cucumber.runtime.model.CucumberFeature.run(CucumberFeature.java:165)\r\n\tat cucumber.api.testng.TestNGCucumberRunner.runCucumber(TestNGCucumberRunner.java:63)\r\n\tat cucumber.api.testng.AbstractTestNGCucumberTests.feature(AbstractTestNGCucumberTests.java:21)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat org.testng.internal.MethodInvocationHelper.invokeMethod(MethodInvocationHelper.java:86)\r\n\tat org.testng.internal.Invoker.invokeMethod(Invoker.java:643)\r\n\tat org.testng.internal.Invoker.invokeTestMethod(Invoker.java:820)\r\n\tat org.testng.internal.Invoker.invokeTestMethods(Invoker.java:1128)\r\n\tat org.testng.internal.TestMethodWorker.invokeTestMethods(TestMethodWorker.java:129)\r\n\tat org.testng.internal.TestMethodWorker.run(TestMethodWorker.java:112)\r\n\tat org.testng.TestRunner.privateRun(TestRunner.java:782)\r\n\tat org.testng.TestRunner.run(TestRunner.java:632)\r\n\tat org.testng.SuiteRunner.runTest(SuiteRunner.java:366)\r\n\tat org.testng.SuiteRunner.runSequentially(SuiteRunner.java:361)\r\n\tat org.testng.SuiteRunner.privateRun(SuiteRunner.java:319)\r\n\tat org.testng.SuiteRunner.run(SuiteRunner.java:268)\r\n\tat org.testng.SuiteRunnerWorker.runSuite(SuiteRunnerWorker.java:52)\r\n\tat org.testng.SuiteRunnerWorker.run(SuiteRunnerWorker.java:86)\r\n\tat org.testng.TestNG.runSuitesSequentially(TestNG.java:1244)\r\n\tat org.testng.TestNG.runSuitesLocally(TestNG.java:1169)\r\n\tat org.testng.TestNG.run(TestNG.java:1064)\r\n\tat org.apache.maven.surefire.testng.TestNGExecutor.run(TestNGExecutor.java:295)\r\n\tat org.apache.maven.surefire.testng.TestNGXmlTestSuite.execute(TestNGXmlTestSuite.java:84)\r\n\tat org.apache.maven.surefire.testng.TestNGProvider.invoke(TestNGProvider.java:90)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.invokeProviderInSameClassLoader(ForkedBooter.java:203)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.runSuitesInProcess(ForkedBooter.java:155)\r\n\tat org.apache.maven.surefire.booter.ForkedBooter.main(ForkedBooter.java:103)\r\n",
+  "status": "failed"
 });
 });
